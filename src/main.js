7ded9a454f0d5c428037669c829d49f35da5ec7c
niuhongkui <397208778@qq.com>
2022-07-18 22:28:21 +0800

    feat():引入elementui

--- src/main.js
+++ src/main.js
@@ -4,7 +4,20 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import axios from 'axios'
+import moment from 'moment'
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
+
+Vue.filter('moment', function (value, formatString) {
+  formatString = formatString || 'YYYY-MM-DD HH:mm:ss'
+  return moment(value).format('YYYY-MM-DD') // value可以是普通日期 20170723
+})
+Vue.prototype.$axios = axios
 
 /* eslint-disable no-new */
 new Vue({
