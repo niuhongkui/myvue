89140cc33f1dfb051531bbf2423fbe4931f9173e
niuhongkui <397208778@qq.com>
2022-07-19 21:27:05 +0800

    feat():router跳转

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
     <el-container>
-      <el-header></el-header>
+      <el-header class="header"></el-header>
       <el-container>
-        <el-aside width="200px" style="background-color: rgb(238, 241, 246);height:80vh">
-          <el-menu :default-openeds="['1', '3']">
+        <el-aside class="aside">
+          <el-menu :default-openeds="['1', '2']">
             <el-submenu index="1">
               <template slot="title"><i class="el-icon-message"></i>导航一</template>
               <el-menu-item-group>
                 <template slot="title">分组一</template>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
+                <el-menu-item @click="navTo('/home')" index="1-1">Home</el-menu-item>
+                <el-menu-item @click="navTo('/')" index="1-2">HelloWorld</el-menu-item>
               </el-menu-item-group>
               <el-menu-item-group title="分组2">
                 <el-menu-item index="1-3">选项3</el-menu-item>
@@ -35,28 +35,13 @@
                 <el-menu-item index="2-4-1">选项4-1</el-menu-item>
               </el-submenu>
             </el-submenu>
-            <el-submenu index="3">
-              <template slot="title"><i class="el-icon-setting"></i>导航三</template>
-              <el-menu-item-group>
-                <template slot="title">分组一</template>
-                <el-menu-item index="3-1">选项1</el-menu-item>
-                <el-menu-item index="3-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="3-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="3-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="3-4-1">选项4-1</el-menu-item>
-              </el-submenu>
-            </el-submenu>
           </el-menu>
         </el-aside>
         <el-container>
-          <el-main>
+          <el-main class="main">
             <router-view />
           </el-main>
-          <el-footer></el-footer>
+          <el-footer class="footer"></el-footer>
         </el-container>
       </el-container>
     </el-container>
@@ -65,7 +50,12 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    navTo (path) {
+      this.$router.push({ path })
+    }
+  }
 }
 </script>
 
@@ -77,5 +67,28 @@ export default {
   text-align: center;
   color: #2c3e50;
   height: 97vh;
+}
+
+.header {
+  background-color: #ddd;
+}
+
+.aside {
+  background-color: #e3e3e3;
+  height: calc(100vh - 70px);
+  width: 200px;
+  overflow: auto;
+}
+
+.aside::-webkit-scrollbar {
+  display: none;
+}
+
+.footer {
+  background-color: #e3e3e3;
+}
+
+.main {
+  background-color: #f3f3f3;
 }
 </style>
